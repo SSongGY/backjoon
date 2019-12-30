d8673c3604dffaacc9f74a7b4f244fde6eff59c5
SSongGY <50525515+SSongGY@users.noreply.github.com>
2019-12-30 11:43:12 +0900

    10799

---
+++ 10799.cpp
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stack>
+using namespace std;
+int main() {
+	char ps[100000];
+	stack<int> st;
+	int count = 0;
+	scanf("%s", ps);
+	for (int i = 0; ps[i] != NULL; i++) {
+		if (ps[i] == '(') {
+			st.push(i);
+		}
+		if (ps[i] == ')') {
+			if (i - st.top() == 1) { // 이건 레이저
+				st.pop();
+				count += st.size();
+			}
+			else {
+				st.pop();
+				count += 1;
+			}
+		}
+	}
+	printf("%d", count);
+	return 0;
+}
