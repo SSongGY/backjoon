6a1e710b03014bbce18f951af9a4b381a5ee75db
SSongGY <50525515+SSongGY@users.noreply.github.com>
2020-01-02 01:34:34 +0900

    2743

---
+++ 2743.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(false);
+	string s;
+	cin >> s;
+	int len = s.length();
+	cout << len;
+	return 0;
+}
